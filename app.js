0a015eacda521a45fccad862c2b19709a14c7964
starytrek <stostostosto@gmail.com>
2024-01-20 12:04:39 +0100

    init project

---
+++ app.js
@@ -0,0 +1,16 @@
+const image = document.getElementById('image1')
+document.addEventListener('click', () => {
+	// console.log('image')
+	image.style.scale = '150%'
+})
+
+// 1. funkcja zmniająca slajd i tekst 1/5
+// 2. funkcja poprzedni slajd
+// 3. i następny slajd
+// 4. blkowanie i wyszareznie przycisku
+// 5. strzałki slajd w lewo czyli funkcja 2. 3.
+
+// Podpowiedzi
+// 1. tablica - do obrazków
+// 2. indexy tablicy do kolejnych i obiekt Math.min math.max
+// 3. css za pomocą JS do rzeczy ruchomych
